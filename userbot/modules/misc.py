c93db4c7a1c7776e5d69514652ae7da5b219c7f4
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-06-27 22:48:32 +0530

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -107,7 +107,7 @@ async def bot_support(wannahelp):
 
 @register(outgoing=True, pattern="^.creator$")
 async def creator(e):
-    await e.edit("[Heyworld](https://github.com/sahyam2019)")
+    await e.edit("[ℜøℽⱥl ẞøℽ ⁪⁬⁮꧁やℜiℽⱥήshu꧂](https://github.com/RoyalBoy69)")
 
 
 @register(outgoing=True, pattern="^.readme$")
