83d769e67bccf0acbf38febdcf21dfedddfb5553
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-07-05 08:13:45 +0530

    Update profile.py

--- userbot/modules/profile.py
+++ userbot/modules/profile.py
@@ -292,7 +292,7 @@ async def fetch_info(replied_user, event):
         "This User has no Username")
     user_bio = "This User has no About" if not user_bio else user_bio
 
-    caption = "<b>USER INFO:</b>\n\n"
+    ccaptionaption = "<b>USER INFO:</b>\n\n"
     caption += f"👨🏻First Name: {first_name}\n"
     caption += f"👨🏻Last Name: {last_name}\n"
     caption += f"👤Username: {username}\n"
@@ -304,21 +304,8 @@ async def fetch_info(replied_user, event):
     caption += f"🆔ID: <code>{user_id}</code>\n\n"
     caption += f"📝Bio: \n<code>{user_bio}</code>\n\n"
     caption += f"🧾Common Chats with this user: {common_chat}\n"
-    caption += f"🔗Permanent Link To Profile: "
-    caption += f"👩‍💻<a href=\"tg://user?id={user_id}\">{first_name}</a>"
-    caption += f"First Name: {first_name}\n"
-    caption += f"Last Name: {last_name}\n"
-    caption += f"Username: {username}\n"
-    caption += f"Data Centre ID: {dc_id}\n"
-    caption += f"Number of Profile Pics: {replied_user_profile_photos_count}\n"
-    caption += f"Is Bot: {is_bot}\n"
-    caption += f"Is Restricted: {restricted}\n"
-    caption += f"Is Verified by Telegram: {verified}\n"
-    caption += f"ID: <code>{user_id}</code>\n\n"
-    caption += f"Bio: \n<code>{user_bio}</code>\n\n"
-    caption += f"Common Chats with this user: {common_chat}\n"
-    caption += "Permanent Link To Profile: "
-    caption += f"<a href=\"tg://user?id={user_id}\">{first_name}</a>"
+    caption += "🔗Permanent Link To Profile: "
+    caption += f"👩‍💻a href=\"tg://user?id={user_id}\">{first_name}</a>"
 
     return photo, caption
 
