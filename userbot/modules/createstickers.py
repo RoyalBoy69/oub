6f4ad357c843d9acac1eaf881791bd2c0f2acfc6
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-07-12 16:15:15 +0530

    Update createstickers.py

--- userbot/modules/createstickers.py
+++ userbot/modules/createstickers.py
@@ -37,7 +37,7 @@ async def sticklet(event):
     draw = ImageDraw.Draw(image)
     fontsize = 230
 
-    FONT_FILE = await get_font_file(event.client, "@Royal_font")
+    FONT_FILE = await get_font_file(event.client, "@FontRes")
 
     font = ImageFont.truetype(FONT_FILE, size=fontsize)
 
