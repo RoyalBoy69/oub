f058078e0ead1155f0803606e688dcf681f253fe
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-07-31 15:05:33 +0530

    Update stickers.py

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -168,8 +168,6 @@ async def kang(event):
         f"{emoji} as the emoji! "
         f"This pack can be found `[here](t.me/addstickers/{packname})",
         parse_mode='md')
-        await asyncio.sleep(7.5)
-        await args.delete()
 
 async def newpack(is_anim, sticker, emoji, packtitle, packname):
     async with bot.conversation('Stickers') as conv:
