6e6c7bc7ab4410e1a3ef751518771090605cc1b1
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-15 20:41:07 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -326,7 +326,7 @@ def paginate_help(page_number, loaded_modules, prefix):
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {} {}".format("🌭", x, "🍔"), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} {}".format("🍆", x, "🍆"), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
