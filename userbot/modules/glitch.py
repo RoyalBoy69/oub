338ea122266d9aa43fcddc6fb22e1625ea9430b3
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-23 14:35:07 +0530

    Update glitch.py

--- userbot/modules/glitch.py
+++ userbot/modules/glitch.py
@@ -13,9 +13,9 @@ from userbot.events import register
 @register(pattern="^.glitch(?: |$)(.*)", outgoing=True)
 async def glitch(cat):
     await cat.edit("```Glitching... 😁```")
-    cmd = cat.pattern_match.group(0)
-    catinput = cat.pattern_match.group(1)
-    reply = await cat.get_reply_message(3)
+    cmd = cat.pattern_match.group()
+    catinput = event.pattern_match.group(2)
+    reply = await cat.get_reply_message()
     if not (reply and (reply.media)):
         await cat.edit("`Media not found...`")
         return
