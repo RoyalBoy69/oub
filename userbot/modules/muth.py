0f11b4d7aa3d2ffc2c767460ca1420a000d7f98a
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-24 11:48:56 +0530

    Uploaded New Plugin

---
+++ userbot/modules/muth.py
@@ -0,0 +1,64 @@
+# ported from X-TRA-TELEGRAM by @RoyalBoyPriyanshu
+
+import random, re
+import asyncio
+from telethon import events
+from userbot.events import register
+from asyncio import sleep
+import time
+from userbot import CMD_HELP
+
+
+@register(pattern="^.muth(?: |$)(.*)", outgoing=True)
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 0.3
+
+    animation_ttl = range(0, 100)
+
+    input_str = event.pattern_match.group(1)
+
+    if input_str == "muth":
+
+        await event.edit(input_str)
+
+        animation_chars = [
+
+            "8✊️===D",
+
+            "8=✊️==D",
+
+            "8==✊️=D",
+
+            "8===✊️D",
+
+            "8==✊️=D",
+
+            "8=✊️==D",
+
+            "8✊️===D",
+
+            "8===✊️D💦",
+
+            "8==✊️=D💦💦",
+
+            "8=✊️==D💦💦💦"
+
+        ]
+
+        for i in animation_ttl:
+        
+            await asyncio.sleep(animation_interval)
+        
+            await event.edit(animation_chars[i % 8])
+
+CMD_HELP.update({
+  "muth":
+   "`.muth`\
+\nUsage: Find yourself."
+})            
