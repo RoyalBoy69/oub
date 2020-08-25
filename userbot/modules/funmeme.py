8630d999c5b4d3c0e6c69948d6d329f45cdd6720
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-25 16:01:45 +0530

    Create funmeme.py

---
+++ userbot/modules/funmeme.py
@@ -0,0 +1,76 @@
+#ported from catuserbot by @RoyalBoyPriyanshu
+''' Whatever Plugin by Noobs of Telegram i.e. @PhycoNinja13b and @Halto_Tha '''
+
+from telethon import events
+import asyncio
+import os
+import sys
+from uniborg.util import admin_cmd
+
+register(pattern=".lmoon")
+async def test(event):
+    if event.fwd_from:
+        return 
+    await event.edit("🌕🌕🌕🌕🌕🌕🌕🌕\n🌕🌕🌖🌔🌖🌔🌕🌕\n🌕🌕🌗🌔🌖🌓🌕🌕\n🌕🌕🌗🌔🌖🌓🌕🌕\n🌕🌕🌖🌓🌗🌔🌕🌕\n🌕🌕🌗🌑🌑🌓🌕🌕\n🌕🌕🌗👀🌑🌓🌕🌕\n🌕🌕🌘👄🌑🌓🌕🌕\n🌕🌕🌗🌑🌑🌒🌕🌕\n🌕🌖🌑🌑🌑🌑🌔🌕\n🌕🌘🌑🌑🌑🌑🌒🌕\n🌖🌑🌑🌑🌑🌑🌑🌔\n🌕🤜🏻🌑🌑🌑🌑🤛🏻🌕\n🌕🌖🌑🌑🌑🌑🌔🌕\n🌘🌑🌑🌑🌑🌑🌑🌒\n🌕🌕🌕🌕🌕🌕🌕🌕")
+
+@register(pattern=".city")
+async def test(event):
+    if event.fwd_from:
+        return 
+    await event.edit("""☁☁🌞      ☁           ☁
+       ☁  ✈         ☁    🚁    ☁    ☁        ☁          ☁     ☁   ☁
+
+🏬🏨🏫🏢🏤🏥🏦🏪🏫
+              🌲/     l🚍\🌳👭
+           🌳/  🚘 l  🏃 \🌴 👬                        👬     🌴/            l  🚔    \🌲
+      🌲/   🚖     l        \        
+          🌳/🚶           |   🚍         \ 🌴🚴🚴
+🌴/                    |                     \🌲""")
+
+# @PhycoNinja13b 's Part begin from here
+
+@register(pattern=".hii")
+async def hi(event):
+    if event.fwd_from:
+        return
+    await event.edit("🌺✨✨🌺✨🌺🌺🌺\n🌺✨✨🌺✨✨🌺✨\n🌺🌺🌺🌺✨✨🌺✨\n🌺✨✨🌺✨✨🌺✨\n🌺✨✨🌺✨🌺🌺🌺\n☁☁☁☁☁☁☁☁")
+
+@register(pattern=".cheer")
+async def cheer(event):
+    if event.fwd_from:
+        return
+    await event.edit("💐💐😉😊💐💐\n☕ Cheer Up  🍵\n🍂 ✨ )) ✨  🍂\n🍂┃ (( * ┣┓ 🍂\n🍂┃*💗 ┣┛ 🍂 \n🍂┗━━┛  🍂🎂 For YOU  🍰\n💐💐😌😚💐💐")
+
+@register(pattern=".getwell")
+async def getwell(event):
+    if event.fwd_from:
+        return
+    await event.edit("🌹🌹🌹🌹🌹🌹🌹🌹 \n🌹😷😢😓😷😢💨🌹\n🌹💝💉🍵💊💐💝🌹\n🌹 GetBetter Soon! 🌹\n🌹🌹🌹🌹🌹🌹🌹🌹")
+
+@register(pattern=".luck")
+async def luck(event):
+    if event.fwd_from:
+        return
+    await event.edit("💚~🍀🍀🍀🍀🍀\n🍀╔╗╔╗╔╗╦╗✨🍀\n🍀║╦║║║║║║👍🍀\n🍀╚╝╚╝╚╝╩╝。 🍀\n🍀・・ⓁⓊⒸⓀ🍀\n🍀🍀🍀 to you💚")
+
+@register(pattern=".sprinkle")
+async def sprinkle(event):
+    if event.fwd_from:
+        return
+    await event.edit("✨.•*¨*.¸.•*¨*.¸¸.•*¨*• ƸӜƷ\n🌸🌺🌸🌺🌸🌺🌸🌺\n Sprinkled with love❤\n🌷🌻🌷🌻🌷🌻🌷🌻\n ¨*.¸.•*¨*. ¸.•*¨*.¸¸.•*¨`*•.✨\n🌹🍀🌹🍀🌹🍀🌹🍀")
+ 
+CMD_HELP.update({
+    "funmeme":
+      "`loon`\
+      \nUsage:Moon meme\
+      \n\n`hii`\
+      \nUsage:Hi message.\
+      \n\n`cheer`\
+      \nUsage:Cheer Up\
+      \n\n`getwell`\
+      \nUsage:Getwell Soon\
+      \n\n`luck`\
+      \nUsage:So lucky\
+      \n\n`sprinkle`
+      \nUsage: Don't khow how it work"
+      })
