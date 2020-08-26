0fc07ff7a36d671a559012796e12a8a654df54bb
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-26 12:20:18 +0530

    Update lovestory.py

--- userbot/modules/lovestory.py
+++ userbot/modules/lovestory.py
@@ -19,7 +19,7 @@ async def _(event):
 
     animation_interval = 3
 
-    animation_ttl = range(0, 103)
+    animation_ttl = range(0, 14)
 
     #input_str = event.pattern_match.group(1)
 
@@ -49,7 +49,7 @@ async def _(event):
 
         await asyncio.sleep(animation_interval)
 
-        await event.edit(animation_chars[i % 103])
+        await event.edit(animation_chars[i % 14])
         
 
 CMD_HELP.update({
