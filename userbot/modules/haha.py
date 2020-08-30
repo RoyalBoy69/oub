2b87c10bd281d3c5e69af3de349d19dcd1634f42
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-30 14:35:03 +0530

    Update haha.py

--- userbot/modules/haha.py
+++ userbot/modules/haha.py
@@ -64,6 +64,92 @@ async def _(event):
         
         await event.edit(animation_chars[i % 11])
         
+@register(pattern=".loveu")
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.5
+    animation_ttl = range(0, 70)
+    await event.edit("loveu")
+    animation_chars = [
+
+            "😀",
+
+            "👩‍🎨",
+
+            "😁",    
+
+            "😂",
+
+            "🤣",
+
+            "😃",
+
+            "😄",
+
+            "😅",
+
+            "😊",
+
+            "☺",
+
+            "🙂",    
+
+            "🤔",
+
+            "🤨",
+
+            "😐",
+
+            "😑",
+
+            "😶",
+
+            "😣",
+
+            "😥",
+
+            "😮",    
+
+            "🤐",
+
+            "😯",
+
+            "😴",
+
+            "😔",
+
+            "😕",
+
+            "☹",
+
+            "🙁",
+
+            "😖",    
+
+            "😞",
+
+            "😟",
+
+            "😢",
+
+            "😭",
+
+            "🤯",
+
+            "💔",
+
+            "❤",
+
+            "i Love You❤",   
+
+        ]
+
+    for i in animation_ttl:
+        await asyncio.sleep(animation_interval)
+    	await event.edit(animation_chars[i % 35])
+
+        
 @register(pattern=".tmoon")        
 async def _(event):
     if event.fwd_from:
