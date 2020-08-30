ff64db4ae26e66aff744e44b73eb2148ef5e858c
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-08-30 13:41:28 +0530

    Update haha.py

--- userbot/modules/haha.py
+++ userbot/modules/haha.py
@@ -135,10 +135,63 @@ async def fuck(e):
                      "`\n╱┃┏━┛╱...┃┃╱┃┃┃┃╱┏┓┃┃ `"
                      "`\n╱┃┃╱.╱.╱ ┃╰━╯┃┃╰━╯┃╱┃┃ `"
                      "`\n╱┗┛╱ ╱ ╱ ╰━━━╯╰━━━╯ ╱╱┗┛ `")
+        
+@register(pattern=".lmoon")
+async def test(event):
+    if event.fwd_from:
+        return 
+    await event.edit("🌕🌕🌕🌕🌕🌕🌕🌕\n🌕🌕🌖🌔🌖🌔🌕🌕\n🌕🌕🌗🌔🌖🌓🌕🌕\n🌕🌕🌗🌔🌖🌓🌕🌕\n🌕🌕🌖🌓🌗🌔🌕🌕\n🌕🌕🌗🌑🌑🌓🌕🌕\n🌕🌕🌗👀🌑🌓🌕🌕\n🌕🌕🌘👄🌑🌓🌕🌕\n🌕🌕🌗🌑🌑🌒🌕🌕\n🌕🌖🌑🌑🌑🌑🌔🌕\n🌕🌘🌑🌑🌑🌑🌒🌕\n🌖🌑🌑🌑🌑🌑🌑🌔\n🌕🤜🏻🌑🌑🌑🌑🤛🏻🌕\n🌕🌖🌑🌑🌑🌑🌔🌕\n🌘🌑🌑🌑🌑🌑🌑🌒\n🌕🌕🌕🌕🌕🌕🌕🌕")
 
+@register(pattern=".city")
+async def test(event):
+    if event.fwd_from:
+        return 
+    await event.edit("""☁☁🌞      ☁           ☁
+       ☁  ✈         ☁    🚁    ☁    ☁        ☁          ☁     ☁   ☁
 
+🏬🏨🏫🏢🏤🏥🏦🏪🏫
+              🌲/     l🚍\🌳👭
+           🌳/  🚘 l  🏃 \🌴 👬                        👬     🌴/            l  🚔    \🌲
+      🌲/   🚖     l        \        
+          🌳/🚶           |   🚍         \ 🌴🚴🚴
+🌴/                    |                     \🌲""")
+
+# @PhycoNinja13b 's Part begin from here
+
+@register(pattern=".hii")
+async def hi(event):
+    if event.fwd_from:
+        return
+    await event.edit("🌺✨✨🌺✨🌺🌺🌺\n🌺✨✨🌺✨✨🌺✨\n🌺🌺🌺🌺✨✨🌺✨\n🌺✨✨🌺✨✨🌺✨\n🌺✨✨🌺✨🌺🌺🌺\n☁☁☁☁☁☁☁☁")
+
+@register(pattern=".cheer")
+async def cheer(event):
+    if event.fwd_from:
+        return
+    await event.edit("💐💐😉😊💐💐\n☕ Cheer Up  🍵\n🍂 ✨ )) ✨  🍂\n🍂┃ (( * ┣┓ 🍂\n🍂┃*💗 ┣┛ 🍂 \n🍂┗━━┛  🍂🎂 For YOU  🍰\n💐💐😌😚💐💐")
+
+@register(pattern=".getwell")
+async def getwell(event):
+    if event.fwd_from:
+        return
+    await event.edit("🌹🌹🌹🌹🌹🌹🌹🌹 \n🌹😷😢😓😷😢💨🌹\n🌹💝💉🍵💊💐💝🌹\n🌹😊GetBetter Soon😊🌹\n🌹🌹🌹🌹🌹🌹🌹🌹")
+
+@register(pattern=".luck")
+async def luck(event):
+    if event.fwd_from:
+        return
+    await event.edit("💚~🍀🍀🍀🍀🍀\n🍀╔╗╔╗╔╗╦╗✨🍀\n🍀║╦║║║║║║👍🍀\n🍀╚╝╚╝╚╝╩╝。 🍀\n🍀 ・ⓁⓊⒸⓀ・ 🍀\n🍀🍀🍀 to you~💚")
+
+@register(pattern=".sprinkle")
+async def sprinkle(event):
+    if event.fwd_from:
+        return
+    await event.edit("✨.•*¨*.¸.•*¨*.¸¸.•*¨*• ƸӜƷ\n🌸🌺🌸🌺🌸🌺🌸🌺\n Sprinkled with love❤\n🌷🌻🌷🌻🌷🌻🌷🌻\n ¨*.¸.•*¨*. ¸.•*¨*.¸¸.•*¨`*•.✨\n🌹🍀🌹🍀🌹🍀🌹🍀")
+    
+    
+    
 CMD_HELP.update({
-  "nope":
+  "fun":
    "`.muth`\
 \nUsage: Find yourself.\
 \n\n`.brain`\
@@ -146,5 +199,17 @@ CMD_HELP.update({
 \n\n`gf`\
 \nUsage:Fu*cing your gf.\
 \n\n`.fuk`\
-\nUsage:Fuck!!"
+\nUsage:Fuck!!\
+\n\n`.lmoon`\
+\nUsage:Moon meme\
+\n\n`.hii`\
+\nUsage:Hi message.\
+\n\n`.cheer`\
+\nUsage:Cheer Up\
+\n\n`.getwell`\
+\nUsage:Getwell Soon\
+\n\n`.luck`\
+\nUsage:So lucky\
+\n\n`.sprinkle`\
+\nUsage: Don't khow how it work"
 })            
