c7dcd7105b1c6980e3b9284641f64956fc0e9424
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-09-14 09:03:29 +0530

    Update getadmin.py

--- userbot/modules/getadmin.py
+++ userbot/modules/getadmin.py
@@ -8,7 +8,7 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.getadmin(?: |$)(.*)")
 async def getadmin(show):
-    if event.fwd_from:
+    if show.fwd_from:
         return
     mentions = "**Admins in this Channel**: \n"
     should_mention_admins = False
