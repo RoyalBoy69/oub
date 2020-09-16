3e9b6931be6340c48540c751ff1ebeab6f901298
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-09-16 21:19:12 +0530

    Uploaded New Plugin

---
+++ userbot/modules/report.py
@@ -0,0 +1,23 @@
+
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot.utils.tools import is_admin
+from userbot.events import register
+
+
+@register(outgoing=True, pattern="^.report(?: |$)(.*)")
+async def admin(event):
+    if event.fwd_from:
+        return
+    mentions = "@admin: 🇸 🇵 🇦 🇲  🇸 🇵 🇴 🇹 🇹 🇪 🇩 "
+    chat = await event.get_input_chat()
+    async for x in event.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f"[\u2063](tg://user?id={x.id})"
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
