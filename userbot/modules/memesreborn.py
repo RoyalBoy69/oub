454f0e0cda34d605e4edbbea62ad4dc3878f97ba
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-09-17 18:28:45 +0530

    Update memesreborn.py

--- userbot/modules/memesreborn.py
+++ userbot/modules/memesreborn.py
@@ -259,7 +259,38 @@ async def fcmd(e):
             out += (n * c) + "\n"
         await e.edit(html.escape(out))
 
-
+@register(pattern="^.color(?: |$)(.*)", outgoing=True)
+async def _(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    message_id = event.message.id
+    if event.reply_to_msg_id:
+        message_id = event.reply_to_msg_id
+    if input_str.startswith("#"):
+        try:
+            usercolor = ImageColor.getrgb(input_str)
+        except Exception as e:
+            await event.edit(str(e))
+            return False
+        else:
+            im = Image.new(mode="RGB", size=(1280, 720), color=usercolor)
+            im.save("remix.png", "PNG")
+            input_str = input_str.replace("#", "#COLOR_")
+            await bot.send_file(
+                event.chat_id,
+                "remix.png",
+                force_document=False,
+                caption=input_str,
+                reply_to=message_id
+            )
+            os.remove("remix.png")
+            await event.delete()
+    else:
+        await event.edit("Syntax: `.color <color_code>` example : `.color #ff0000`")
+	
+	
+	
 CMD_HELP.update({
     "memesreborn":
     "`.fuk`\
@@ -267,6 +298,8 @@ CMD_HELP.update({
 \n\n`.iwi`\
 \nUsage: gib text and see magik.\
 \n\n`.rape` `.thanos` `.chu` `.abuse` `.abusehard` `.gtfo` `.F` `.upro` `.nikal` `iwi` `.\
-\nUsage: See it yourself nibba🌚."
+\nUsage: See it yourself nibba🌚.\
+\n\n`.color` <color code>\
+\nUsage: example : `.color #ff0000`"
 
 })
