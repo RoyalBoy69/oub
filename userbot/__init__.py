abf9b5ea2b57df70a616383256869de46d08752e
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-09-17 18:15:43 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -318,12 +318,12 @@ AFKREASON = None
 
 
 def paginate_help(page_number, loaded_modules, prefix):
-    number_of_rows = 5
+    number_of_rows = 7
     number_of_cols = 2
     helpable_modules = [p for p in loaded_modules if not p.startswith("_")]
     helpable_modules = sorted(helpable_modules)
     modules = [
-        custom.Button.inline("{} {}".format("🔹", x), data="ub_modul_{}".format(x))
+        custom.Button.inline("{} {} {}".format("⚔️", x,"⚔️), data="ub_modul_{}".format(x))
         for x in helpable_modules
     ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
@@ -362,7 +362,7 @@ with bot:
         @tgbot.on(events.NewMessage(pattern="/start"))
         async def handler(event):
             if event.message.from_id != uid:
-                await event.reply("I'm [oub-remix](https://github.com/sahyam2019/oub-remix) modules helper...\nplease make your own bot, don't use mine 😋")
+                await event.reply("I'm [oub-remix](https://github.com/RoyalBoy69/oub-remix) modules helper...\nplease make your own bot, don't use mine 😋")
             else:
                 await event.reply(f"`Hey there {ALIVE_NAME}\n\nI work for you :)`")
 
@@ -419,7 +419,7 @@ with bot:
                 # https://t.me/TelethonChat/115200
                 await event.edit(buttons=buttons)
             else:
-                reply_pop_up_alert = "Please make for yourself, don't use my bot!"
+                reply_pop_up_alert = "Abe kiu dusre ka  bot use kr rha khud ka bana pta nhi kha se ata h kaise log h chal futt ab"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
         @tgbot.on(
@@ -437,7 +437,7 @@ with bot:
                 # https://t.me/TelethonChat/115200
                 await event.edit(buttons=buttons)
             else:
-                reply_pop_up_alert = "Please make for yourself, don't use my bot!"
+                reply_pop_up_alert = "Abe khud ka bot bana mera use karne ki aukat nhi h teri chal ja ab"
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
         @tgbot.on(
