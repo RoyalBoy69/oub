2815826ae507771a8f59baf2a9fd88fd6884aaf3
ℜøℽⱥl ẞøℽ ⁪⁬⁮やℜiℽⱥήshu <64057443+RoyalBoy69@users.noreply.github.com>
2020-09-27 12:19:23 +0530

    Update wh.py

--- userbot/modules/wh.py
+++ userbot/modules/wh.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
 from userbot.utils.tools import is_admin
-
+from userbot.events import register
 
 @register(outgoing=True, pattern="^.wh(?: |$)(.*)")
 async def who(event):
